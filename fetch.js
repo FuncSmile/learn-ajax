13273564e63d81f034b722dde4806b868ae09ce7
FuncSmile <user.fadli3001@gmail.com>
2025-05-26 00:59:59 +0700

    change filename and create file fetch.js

---
+++ fetch.js
@@ -0,0 +1,13 @@
+fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
