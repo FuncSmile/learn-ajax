d9d4840f3b18ce2085302b48332ad1a6bb987a43
FuncSmile <user.fadli3001@gmail.com>
2025-05-26 01:12:55 +0700

    fetch with async await function

--- fetch.js
+++ fetch.js
@@ -1,13 +1,28 @@
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-  .then((response) => {
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
+let data;
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
+    data = await response.json();
+    return console.log(data);
+  } catch (error) {
     console.error("Error:", error);
-  });
+  }
+}
+
+fetchData("https://pokeapi.co/api/v2/pokemon/squirtle");
